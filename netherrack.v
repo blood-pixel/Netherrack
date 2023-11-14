988dd7e008cd7cb5e3835cef37784d59cd0c1c65
scuderia666 <satouuxd@gmail.com>
2023-11-14 23:45:43 +0300

    main

--- netherrack.v
+++ netherrack.v
@@ -1,9 +1,17 @@
 module main
 
+import network {Network}
+
 pub struct Netherrack {
+mut:
+    network Network
 }
 
 pub fn (mut n Netherrack) start() {
+    println("Netherrack is starting...")
+
+    n.network = Network{}
+    n.network.init()
 }
 
 fn main() {
---
+++ network/network.v
@@ -0,0 +1,9 @@
+module network
+
+pub struct Network {
+
+}
+
+pub fn (mut n Network) init() {
+    println("Network is starting")
+}
