d37468cf3ea0bac4c0a145fbca28216f423b8460
Furkan Can Mera <schermann470@outlook.com>
2023-11-15 01:30:26 +0300

    test

--- network/network.v
+++ network/network.v
@@ -1,9 +1,21 @@
 module network
 
+import net
+
+const port = 19132
+
 pub struct Network {
 
 }
 
 pub fn (mut n Network) init() {
     println("Network is starting")
+
+
+    mut c := net.listen_udp(':${port}') or { return }
+
+    mut buf := []u8{len: 100}
+    for {
+        read, addr := c.read(mut buf) or { continue }
+    }
 }
